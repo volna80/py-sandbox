ed7d1bb37cef808dfa2dc0b74d1a3f2d727a8ec5
Nikolay Volnov <volna80@gmail.com>
2019-11-21 12:49:04 +0300

    solved check and mate

--- codewars/SolutionTest.py
+++ codewars/SolutionTest.py
@@ -1,61 +1,106 @@
 import codewars_test as test
 from codewars.Solution_check_and_mate import isCheck
+from codewars.Solution_check_and_mate import isMate
 
 pieces = [
-  {'piece': "king", 'owner': 1, 'x': 4, 'y': 0},
-  {'piece': "king", 'owner': 0, 'x': 4, 'y': 7},
-  {'piece': "pawn", 'owner': 1, 'x': 5, 'y': 6}
-]
-test.assert_equals(isCheck(pieces, 0), [pieces[2]], "Pawn threatens king")
+  {'x': 4, 'y': 0, 'owner': 1, 'piece': 'king'},
+  {'y': 4, 'x': 1, 'owner': 1, 'prevX': 3, 'prevY': 2, 'piece': 'bishop'},
+  {'x': 0, 'y': 7, 'owner': 1, 'piece': 'queen'},
+  {'x': 4, 'y': 6, 'owner': 0, 'piece': 'pawn'},
+  {'x': 5, 'y': 6, 'owner': 0, 'piece': 'pawn'},
+  {'x': 1, 'y': 7, 'owner': 0, 'piece': 'rook'},
+  {'x': 3, 'y': 7, 'owner': 0, 'piece': 'bishop'},
+  {'x': 4, 'y': 7, 'owner': 0, 'piece': 'king'},
+  {'x': 5, 'y': 7, 'owner': 0, 'piece': 'rook'}]
 
-pieces = [
-  {'piece': "king", 'owner': 1, 'x': 4, 'y': 0},
-  {'piece': "king", 'owner': 0, 'x': 4, 'y': 7},
-  {'piece': "rook", 'owner': 1, 'x': 4, 'y': 1}
-]
-test.assert_equals(isCheck(pieces, 0), [pieces[2]], "Rook threatens king")
+test.assert_equals(isCheck(pieces, 0), [pieces[1]], "bishop threatens king")
+test.assert_equals(isMate(pieces, 0), False)
 
-
-pieces = [
-  {'piece': "king", 'owner': 1, 'x': 4, 'y': 0},
-  {'piece': "king", 'owner': 0, 'x': 4, 'y': 7},
-  {'piece': "knight", 'owner': 1, 'x': 2, 'y': 6}
-]
-test.assert_equals(isCheck(pieces, 0), [pieces[2]], "Knight threatens king")
-
-pieces = [
-  {'piece': "king", 'owner': 1, 'x': 4, 'y': 0},
-  {'piece': "king", 'owner': 0, 'x': 4, 'y': 7},
-  {'piece': "bishop", 'owner': 1, 'x': 0, 'y': 3}
-]
-test.assert_equals(isCheck(pieces, 0), [pieces[2]], "Bishop threatens king")
-
-pieces = [
-  {'piece': "king", 'owner': 1, 'x': 4, 'y': 0},
-  {'piece': "king", 'owner': 0, 'x': 4, 'y': 7},
-  {'piece': "queen", 'owner': 1, 'x': 4, 'y': 1}
-]
-test.assert_equals(isCheck(pieces, 0), [pieces[2]], "Queen threatens king")
-
-pieces = [
-  {'piece': "king", 'owner': 1, 'x': 4, 'y': 0},
-  {'piece': "king", 'owner': 0, 'x': 4, 'y': 7},
-  {'piece': "queen", 'owner': 1, 'x': 7, 'y': 4}
-]
-test.assert_equals(isCheck(pieces, 0), [pieces[2]], "Queen threatens king")
-
-pieces = [
-  {'piece': "king", 'owner': 1, 'x': 4, 'y': 0},
-  {'piece': "pawn", 'owner': 0, 'x': 4, 'y': 6},
-  {'piece': "pawn", 'owner': 0, 'x': 5, 'y': 6},
-  {'piece': "king", 'owner': 0, 'x': 4, 'y': 7},
-  {'piece': "bishop", 'owner': 0, 'x': 5, 'y': 7},
-  {'piece': "bishop", 'owner': 1, 'x': 1, 'y': 4},
-  {'piece': "rook", 'owner': 1, 'x': 2, 'y': 7, 'prevX': 2, 'prevY': 5}
-]
-
-def sortFunc(a,b):
-  if(a['y'] == b['y']): return a['x'] - b['x']
-  return a['y'] - b['y']
-
-test.assert_equals(sorted(isCheck(pieces, 0),key=lambda x: x['y']), [pieces[5], pieces[6]], "Double threat")
+# pieces = [
+#   {'y': 3, 'owner': 1, 'piece': 'king', 'x': 5},
+#   {'y': 4, 'prevY': 6, 'owner': 0, 'prevX': 4, 'piece': 'pawn', 'x': 4},
+#   {'y': 6, 'owner': 0, 'piece': 'pawn', 'x': 5},
+#   {'y': 7, 'owner': 0, 'piece': 'king', 'x': 4},
+#   {'y': 5, 'owner': 0, 'piece': 'knight', 'x': 2},
+#   {'y': 4, 'owner': 1, 'piece': 'pawn', 'x': 3},
+#   {'y': 3, 'owner': 1, 'piece': 'knight', 'x': 3},
+#   {'y': 3, 'owner': 1, 'piece': 'pawn', 'x': 4},
+#   {'y': 2, 'owner': 1, 'piece': 'bishop', 'x': 4},
+#   {'y': 2, 'owner': 1, 'piece': 'rook', 'x': 5},
+#   {'y': 5, 'owner': 0, 'piece': 'queen', 'x': 6}]
+#
+# test.assert_equals(isCheck(pieces, 1), [pieces[1]], "pawn threatens king")
+# test.assert_equals(isMate(pieces, 1), False)
+#
+#
+# pieces = [
+#   {'piece': "king", 'owner': 1, 'x': 3, 'y': 6},
+#   {'piece': "king", 'owner': 0, 'x': 4, 'y': 7}
+# ]
+# test.assert_equals(isCheck(pieces, 0), [pieces[0]], "King threatens king")
+# test.assert_equals(isMate(pieces, 0), False)
+#
+# pieces = [
+#   {'piece': "king", 'owner': 1, 'x': 4, 'y': 0},
+#   {'piece': "king", 'owner': 0, 'x': 4, 'y': 7},
+#   {'piece': "pawn", 'owner': 1, 'x': 5, 'y': 6}
+# ]
+# test.assert_equals(isCheck(pieces, 0), [pieces[2]], "Pawn threatens king")
+# test.assert_equals(isMate(pieces, 0), False)
+#
+# pieces = [
+#   {'piece': "king", 'owner': 1, 'x': 4, 'y': 0},
+#   {'piece': "king", 'owner': 0, 'x': 4, 'y': 7},
+#   {'piece': "rook", 'owner': 1, 'x': 4, 'y': 1}
+# ]
+# test.assert_equals(isCheck(pieces, 0), [pieces[2]], "Rook threatens king")
+# test.assert_equals(isMate(pieces, 0), False)
+#
+#
+# pieces = [
+#   {'piece': "king", 'owner': 1, 'x': 4, 'y': 0},
+#   {'piece': "king", 'owner': 0, 'x': 4, 'y': 7},
+#   {'piece': "knight", 'owner': 1, 'x': 2, 'y': 6}
+# ]
+# test.assert_equals(isCheck(pieces, 0), [pieces[2]], "Knight threatens king")
+# test.assert_equals(isMate(pieces, 0), False)
+#
+# pieces = [
+#   {'piece': "king", 'owner': 1, 'x': 4, 'y': 0},
+#   {'piece': "king", 'owner': 0, 'x': 4, 'y': 7},
+#   {'piece': "bishop", 'owner': 1, 'x': 0, 'y': 3}
+# ]
+# test.assert_equals(isCheck(pieces, 0), [pieces[2]], "Bishop threatens king")
+# test.assert_equals(isMate(pieces, 0), False)
+#
+# pieces = [
+#   {'piece': "king", 'owner': 1, 'x': 4, 'y': 0},
+#   {'piece': "king", 'owner': 0, 'x': 4, 'y': 7},
+#   {'piece': "queen", 'owner': 1, 'x': 4, 'y': 1}
+# ]
+# test.assert_equals(isCheck(pieces, 0), [pieces[2]], "Queen threatens king")
+# test.assert_equals(isMate(pieces, 0), False)
+#
+# pieces = [
+#   {'piece': "king", 'owner': 1, 'x': 4, 'y': 0},
+#   {'piece': "king", 'owner': 0, 'x': 4, 'y': 7},
+#   {'piece': "queen", 'owner': 1, 'x': 7, 'y': 4}
+# ]
+# test.assert_equals(isCheck(pieces, 0), [pieces[2]], "Queen threatens king")
+# test.assert_equals(isMate(pieces, 0), False)
+#
+# pieces = [
+#   {'piece': "king", 'owner': 1, 'x': 4, 'y': 0},
+#   {'piece': "pawn", 'owner': 0, 'x': 4, 'y': 6},
+#   {'piece': "pawn", 'owner': 0, 'x': 5, 'y': 6},
+#   {'piece': "king", 'owner': 0, 'x': 4, 'y': 7},
+#   {'piece': "bishop", 'owner': 0, 'x': 5, 'y': 7},
+#   {'piece': "bishop", 'owner': 1, 'x': 1, 'y': 4},
+#   {'piece': "rook", 'owner': 1, 'x': 2, 'y': 7, 'prevX': 2, 'prevY': 5}
+# ]
+#
+# def sortFunc(a,b):
+#   if(a['y'] == b['y']): return a['x'] - b['x']
+#   return a['y'] - b['y']
+#
+# test.assert_equals(sorted(isCheck(pieces, 0),key=lambda x: x['y']), [pieces[5], pieces[6]], "Double threat")
--- codewars/Solution_check_and_mate.py
+++ codewars/Solution_check_and_mate.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-dt = np.dtype([('type', np.unicode_), ('owner',np.int_)])
+dt = np.dtype([('type', '<U5'), ('owner', np.int_)])
 
 # Returns an array of threats if the arrangement of
 # the pieces is a check, otherwise false
@@ -16,7 +16,6 @@ def isCheck(pieces, player):
         board[p['x'], p['y']]['owner'] = p['owner']
         if p['owner'] == player and p['piece'] == 'king':
             player_king = convert_to_point(p)
-
 
     threats = []
 
@@ -41,17 +40,29 @@ def out_of_range(point):
     return point[0] < 0 or point[0] >= 8 or point[1] < 0 or point[1] >= 8
 
 
+#return True if a cell is empty
+def is_empty(board, xy):
+    return board[xy[0], xy[1]]['owner'] == -1
+
+
+def is_not_empty(board, xy):
+    return not is_empty(board, xy)
+
+
 def nobody_block(start, end, step, board):
     while not out_of_range(start):
         start = start + step
         if np.array_equal(start, end):
             return True
-        if board[tuple(start.tolist())]['owner'] != -1:
+        if is_not_empty(board, start):
             return False
     return False
 
 
 def king_attacks(self, enemy_king, board, player_white):
+    if abs(self[0] - enemy_king[0]) <= 1 and abs(self[1] - enemy_king[1]) <=1:
+        return True
+
     return False
 
 
@@ -91,7 +102,7 @@ def knight_attacks(self, target, board, player_white):
 def pawn_attacks(self, enemy_king, board, player_white):
 
     if abs(self[0] - enemy_king[0]) == 1 and \
-            self[1] - enemy_king[1] == -1 if player_white else 1:
+            self[1] - enemy_king[1] == (-1 if player_white else 1):
         return True
 
     return False
@@ -100,4 +111,174 @@ def pawn_attacks(self, enemy_king, board, player_white):
 # Returns true if the arrangement of the
 # pieces is a check mate, otherwise false
 def isMate(pieces, player):
-    pass
+
+    pieces = pieces.copy()
+
+    if len(isCheck(pieces, player)) == 0:
+        return False
+
+    board = np.full((8, 8), -1, dtype=dt)
+    en_passant = None
+
+    for p in pieces:
+        board[p['x'], p['y']]['type'] = p['piece']
+        board[p['x'], p['y']]['owner'] = p['owner']
+        if p['piece'] == 'pawn' and p.get('prevX') is not None:
+            en_passant = p
+
+    moves = {'king': king_move, 'rook': rook_move, 'bishop': bishop_move, 'queen': queen_move,
+             'knight': knight_move, 'pawn': pawn_move}
+
+    for i in range(len(pieces)):
+        p = pieces[i]
+        if p['owner'] != player:
+            continue
+        xy = convert_to_point(p)
+        if not moves[p['piece']](p.copy(), xy, pieces[:i] + pieces[i+1:], player, board, en_passant):
+            return False
+
+    return True
+
+# True if there is no move which save a king from a check
+# Otherwise false
+def king_move(self, xy, pieces, player, board, en_passant):
+    return king_or_knight_move(self, xy, pieces, player, board, [np.array([1,-1]), np.array([1,0]), np.array([1,-1]),
+                np.array([-1,-1]), np.array([-1,0]), np.array([-1,1]),
+                np.array([0,1]), np.array([0,-1])])
+
+
+def king_or_knight_move(self, xy, pieces, player, board, steps):
+    for dxy in steps:
+        newxy = xy + dxy
+        if out_of_range(newxy):
+            continue
+        if board[newxy[0], newxy[1]]['owner'] == player:
+            continue
+        elif is_not_empty(board, newxy):
+            #attack
+            pieces2 = pieces.copy()
+            for i in range(len(pieces2)):
+                if pieces2[i]['x'] == newxy[0] and pieces2[i]['y'] == newxy[1]:
+                    pieces2.remove(pieces2[i])
+                    break
+
+            self['x'] = newxy[0]
+            self['y'] = newxy[1]
+            if len(isCheck(pieces2 + [self], player)) == 0:
+                return False
+        else:
+            self['x'] = newxy[0]
+            self['y'] = newxy[1]
+            if len(isCheck(pieces + [self], player)) == 0:
+                return False
+
+    return True
+
+def rook_or_bishop_move(self, xy, pieces, player, board, steps):
+    for dxy in steps:
+        newxy = xy
+        while True:
+            newxy = newxy + dxy
+            if out_of_range(newxy):
+                break
+            if board[newxy[0], newxy[1]]['owner'] == player:
+                break
+            elif is_not_empty(board, newxy):
+                # attack
+                pieces2 = pieces.copy()
+                for i in range(len(pieces2)):
+                    if pieces2[i]['x'] == newxy[0] and pieces2[i]['y'] == newxy[1]:
+                        pieces2.remove(pieces2[i])
+                        break
+                self['x'] = newxy[0]
+                self['y'] = newxy[1]
+                if len(isCheck(pieces2 + [self], player)) == 0:
+                    return False
+            else:
+                self['x'] = newxy[0]
+                self['y'] = newxy[1]
+                if len(isCheck(pieces + [self], player)) == 0:
+                    return False
+                else:
+                    continue
+
+    return True
+
+
+def rook_move(self, xy, pieces, player, board, en_passant):
+    return rook_or_bishop_move(self, xy, pieces, player, board, [np.array([1,0]), np.array([0,1]), np.array([-1,0]), np.array([0,-1])])
+
+
+def bishop_move(self, xy, pieces, player, board, en_passant):
+    return rook_or_bishop_move(self, xy, pieces, player, board, [np.array([1, 1]), np.array([-1, 1]), np.array([-1, -1]), np.array([1, -1])])
+
+
+def queen_move(self, xy, pieces, player, board, en_passant):
+    if not rook_move(self, xy, pieces, player, board, en_passant):
+        return False
+    return bishop_move(self, xy, pieces, player, board, en_passant)
+
+
+def knight_move(self, xy, pieces, player, board, en_passant):
+    return king_or_knight_move(self, xy, pieces, player, board, [
+        np.array([-2, 1]),np.array([-1, 2]),np.array([1, 2]),np.array([2, 1]),
+        np.array([2, -1]),np.array([1, -2]),np.array([-1, -2]),np.array([-2, -1])]
+                               )
+
+
+def pawn_move(self, xy, pieces, player, board, en_passant):
+    player_white = player == 0
+    enemy = 1 if player_white else 1
+
+    steps = [np.array([0, 1]) if not player_white else np.array([0, -1])]
+
+    # an additional move if a pown does not move
+    if player_white and self['y'] == 6:
+        steps = steps + [np.array([0, -2])]
+    elif not player_white and self['y'] == 1:
+        steps = steps + [np.array([0, 2])]
+
+    for dxy in steps:
+        newxy = xy + dxy
+        if not out_of_range(newxy) and is_empty(board, newxy):
+            #can move
+            self['x'] = newxy[0]
+            self['y'] = newxy[1]
+            if len(isCheck(pieces + [self], player)) == 0:
+                return False
+
+    #check if we can attack somebody
+
+    steps = [np.array([1, 1]), np.array([-1, 1])] if not player_white else [np.array([1, -1]), np.array([1, -1])]
+
+    for dxy in steps:
+        newxy = xy + dxy
+        if out_of_range(newxy):
+            continue
+        if board[newxy[0], newxy[1]]['owner'] == enemy:
+            #attack
+            pieces2 = pieces.copy()
+            for i in range(len(pieces2)):
+                if pieces2[i]['x'] == newxy[0] and pieces2[i]['y'] == newxy[1]:
+                    pieces2.remove(pieces2[i])
+                    break
+
+            self['x'] = newxy[0]
+            self['y'] = newxy[1]
+            if len(isCheck(pieces2 + [self], player)) == 0:
+                return False
+
+        if en_passant is not None and en_passant['prevX'] == newxy[0] and (en_passant['prevY'] + (-1 if not player_white else 1)) == newxy[1]:
+            pieces2 = pieces.copy()
+            for i in range(len(pieces2)):
+                if pieces2[i]['x'] == en_passant['x'] and pieces2[i]['y'] == en_passant['y']:
+                    pieces2.remove(pieces2[i])
+                    break
+
+            self['x'] = newxy[0]
+            self['y'] = newxy[1]
+            if len(isCheck(pieces2 + [self], player)) == 0:
+                return False
+
+    return True
+
